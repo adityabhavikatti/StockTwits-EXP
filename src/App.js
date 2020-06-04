ec5f4953d5a249e43a0f5c5641123fe08c3c680d
adityabhavikatti <aditya.bhavikatti@gmail.com>
2020-06-04 16:12:58 -0700

    Testing

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+//import Background from "./components/background"
+import axios from 'axios';
+import './App.css';
+import './styles.css'
+
+function App() {
+  const [stox, setStox] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://api.stocktwits.com/api/2/streams/symbol/amzn.json'
+    );
+    setStox(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Stocktwits Tweets</h1>
+      <h2>Fetch tweets from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="tweets">
+        {stox &&
+          stox.messages.map((tweet, index) => {
+            const cleanedDate = new Date(tweet.created_at).toDateString();
+
+            return (
+              <div className="tweet" key={index}>
+                <h2>Tweet {index + 1}</h2>
+                <h3>$AMZN</h3>
+
+                <div className="details">
+                  <p>👨: {tweet.user.name}</p>
+                  <p>📖: {tweet.body}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
