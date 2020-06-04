f1e2931855a7a3c36f3f4aeb7feeb9f469775018
adityabhavikatti <aditya.bhavikatti@gmail.com>
2020-06-04 16:22:00 -0700

    Updates

---
+++ precache-manifest.8596f58bbe18be932c4fd54da1befb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5508cf106aeb4c0a2949e8b31b05dc",
+    "url": "/StockTwits-EXP/index.html"
+  },
+  {
+    "revision": "cc4ba58eac544ff3869f",
+    "url": "/StockTwits-EXP/static/css/main.06d97062.chunk.css"
+  },
+  {
+    "revision": "415df1d8aff194b84b98",
+    "url": "/StockTwits-EXP/static/js/2.85540902.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StockTwits-EXP/static/js/2.85540902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4ba58eac544ff3869f",
+    "url": "/StockTwits-EXP/static/js/main.d49d65d2.chunk.js"
+  },
+  {
+    "revision": "aa679862943b947a670a",
+    "url": "/StockTwits-EXP/static/js/runtime-main.c691880d.js"
+  }
+]);
